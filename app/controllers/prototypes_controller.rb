b200c8b0b63449fd38c91d9abce68e11777ebf46
mocchio <skmt.rome@gmail.com>
2023-03-01 14:31:35 +0900

    トップページへのリダイレクト設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :move_to_index, only: :edit
+
   def index
     @prototypes = Prototype.all 
   end
@@ -45,5 +47,11 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
