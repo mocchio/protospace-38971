9e6e155c2f519d9448b1cc866c6376f4ee84dab9
mocchio <skmt.rome@gmail.com>
2023-03-02 19:56:36 +0900

    直接編集ページに行けないようにする

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
   end
 
   def update
